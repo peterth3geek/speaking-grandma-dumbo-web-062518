dfd72733a73fe51bd5748e8127b021265843ebcd
Peter Hargarten <peterwhargarten@gmail.com>
2018-06-19 17:28:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,6 @@ if phrase == "I LOVE YOU GRANDMA!"
     p "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase
     p "NO, NOT SINCE 1938!"
-else puts "HUH?! SPEAK UP, SONNY!"
+else p "HUH?! SPEAK UP, SONNY!"
 end
 end
